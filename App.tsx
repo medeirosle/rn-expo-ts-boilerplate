5231e0cf0d3e66594ee486279bc884aa0e689515
Pablo Veiga <vcpablo@Pablos-MacBook-Pro.local>
2020-03-02 20:26:02 -0300

    feature(graphql-client): introduce apollo client

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { render } from 'react-dom'
 import { StyleSheet, Text, View } from 'react-native'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from '@apollo/react-hooks'
+import ApolloClient from './lib/apollo'
+
+import Login from './components/Login'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ApolloProvider client={ApolloClient}>
+      <ApolloHooksProvider client={ApolloClient}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <Login />
+        </View>
+      </ApolloHooksProvider>
+    </ApolloProvider>
   )
 }
 
@@ -17,3 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
+
+render(App(), document.getElementById('root'))
